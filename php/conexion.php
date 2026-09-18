8f06f72cd1b6259a66b5194c6aab65e46069fde8
gomezelindall-ui <gomezelindall@gmail.com>
2026-09-17 21:32:28 -0500

    cambio7

--- php/conexion.php
+++ php/conexion.php
@@ -5,10 +5,10 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
 // Configuración de conexión
-$host = 'mysql-jairoapi.alwaysdata.net';
-$db = 'jairoapi_envios_repositorio';
-$user = 'jairoapi';
-$password = 'clase1234';
+$host = 'ftp-elindall.alwaysdata.net';
+$db = 'elindall_biblioteca';
+$user = 'elindall';
+$password = 'jhosep2020';
 $charset = 'utf8mb4';
 
 try {
